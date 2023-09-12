76835485bc80477fc9ab03a71badd6b660eaf407
kwalamei <kualameik@gmail.com>
2023-09-12 06:18:19 -0700

    updates

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints aline using printf function
+ * Return: 0 (This is a success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+		return (0);
+}
